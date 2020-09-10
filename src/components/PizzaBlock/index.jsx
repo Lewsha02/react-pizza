4a409018c502fbcb98cf6603b61c2f7c9049c88b
Lewsha02 <egorney2002@gmail.com>
2020-09-10 16:18:44 +0300

    add pizzas to redux

--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import Button from '../Button';
 
-function PizzaBlock({ name, imageUrl, price, types, sizes }) {
+function PizzaBlock({ id, name, imageUrl, price, types, sizes, onClickAddPizza, addedCount }) {
 	const availableTypes = ['тонкое', 'традиционное'];
-	// TODO: Проверить почему не выбирается по умолчанию первый размер при первом ренедере
 	const availableSizes = [26, 30, 40];
 
 	const [activeType, setActiveType] = React.useState(types[0]);
-	const [activeSize, setActiveSize] = React.useState(sizes[0]);
+	const [activeSize, setActiveSize] = React.useState(0);
 
 	const onSelectType = (index) => {
 		setActiveType(index);
@@ -16,6 +16,18 @@ function PizzaBlock({ name, imageUrl, price, types, sizes }) {
 
 	const onSelectSize = (index) => {
 		setActiveSize(index);
+	};
+
+	const onAddPizza = () => {
+		const obj = {
+			id,
+			name,
+			imageUrl,
+			price,
+			size: availableSizes[activeSize],
+			type: availableTypes[activeType]
+		};
+		onClickAddPizza(obj);
 	};
 
 	return (
@@ -52,7 +64,10 @@ function PizzaBlock({ name, imageUrl, price, types, sizes }) {
 			</div>
 			<div className="pizza-block__bottom">
 				<div className="pizza-block__price">от {price} ₽</div>
-				<div className="button button--outline button--add">
+				<Button
+					onClick={onAddPizza}
+					className="button--add"
+					outline>
 					<svg
 						width="12"
 						height="12"
@@ -65,8 +80,8 @@ function PizzaBlock({ name, imageUrl, price, types, sizes }) {
 						/>
 					</svg>
 					<span>Добавить</span>
-					<i>2</i>
-				</div>
+					{addedCount && <i>{addedCount}</i>}
+				</Button>
 			</div>
 		</div>
 	);
@@ -78,6 +93,8 @@ PizzaBlock.propTypes = {
 	price: PropTypes.number,
 	types: PropTypes.arrayOf(PropTypes.number),
 	sizes: PropTypes.arrayOf(PropTypes.number),
+	onAddPizza: PropTypes.func,
+	addedCount: PropTypes.number,
 };
 
 PizzaBlock.defaultProps = {
